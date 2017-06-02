aa1d84c2b234d4fa21de1479fe79ff49039035b7
pblaas <pblaas@it-ernity.nl>
2017-06-02 17:18:30 +0200

    update index code

--- app/index.php
+++ app/index.php
@@ -31,7 +31,7 @@
     </nav>
   <div class="container">
     <div class="row">
-      <div class="col-md-4">
+      <div class="col-md-4 offset-sm-4">
         <?php
         echo "HTTP_X_FORWARDED_HOST: ". $_SERVER['HTTP_X_FORWARDED_HOST']."<br /><br />";
         echo "Webserver: ". $_SERVER['SERVER_ADDR']."<br />";
@@ -45,7 +45,7 @@
       </div>
     </div>
     <div class="row">
-      <div class="col-md-6">
+      <div class="col-md-4 offset-sm-4">
         <img src="nginx-phpfpm_architecture.jpg" height="auto" width=100%>
       </div>
     </div>
